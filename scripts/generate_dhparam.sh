ab8c0d153321824edc0ad417f816b197afbdaec3
Richard Boldiš <richard@boldis.dev>
2021-03-01 20:31:07 +0100

    Update size of dhparam to 2048

--- scripts/generate_dhparam.sh
+++ scripts/generate_dhparam.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 DHPARAM_PATH=/etc/nginx/ssl/keys/dhparam.pem
+DHPARAM_SIZE=2048
 
 if [[ ! -f "${DHPARAM_PATH}" ]]; then
-    openssl dhparam -out "${DHPARAM_PATH}" 4096
+    openssl dhparam -out "${DHPARAM_PATH}" "${DHPARAM_SIZE}"
 fi
